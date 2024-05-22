af718082895e8a6674122f050e123c16f3db06fe
tanny <tannyapriliani271@gmail.com>
2024-05-22 20:10:50 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="app">
+    <header>
+      <h1>Pencatatan Sipil</h1>
+    </header>
+    <expense-form @add-expense="addExpense"></expense-form>
+    <expense-list :expenses="expenses">
+      <expense-item-slot></expense-item-slot>
+    </expense-list>
+    <div class="totals">
+      <h2>Jumlah Keseluruhan Pencatatan: {{ totalExpense }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+import ExpenseForm from './components/ExpenseForm.vue';
+import ExpenseList from './components/ExpenseList.vue';
+import ExpenseItemSlot from './components/ExpenseItemSlot.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ExpenseForm,
+    ExpenseList,
+    ExpenseItemSlot
+  },
+  data() {
+    return {
+      expenses: []
+    };
+  },
+  computed: {
+    totalExpense() {
+      return this.expenses.reduce((total, expense) => total + expense.total, 0);
+    }
+  },
+  methods: {
+    addExpense(expense) {
+      this.expenses.push(expense);
+    }
+  }
+}
+</script>
+
